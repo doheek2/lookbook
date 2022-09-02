f5cc1a29e1e99bc76ce8cd8e6b5ef1df0a35b7d9
doheek2 <doheek2@naver.com>
2022-09-02 23:56:39 +0900

    feat: kakao api 연결 및 첫번째 commit

--- src/routes/Main/index.tsx
+++ src/routes/Main/index.tsx
@@ -1,5 +1,41 @@
-const TodoList = () => {
-  return <div>foundation</div>
+import { ChangeEvent, useCallback, useState } from 'react'
+import { getBookList } from 'utils/api'
+
+const Main = () => {
+  const [book, setBook] = useState([])
+  const [query, setQuery] = useState('')
+
+  const getbookAPIHandler = useCallback(
+    async (query: string, reset: boolean) => {
+      const params = {
+        query,
+        sort: 'accuracy',
+        page: 1,
+        size: 10,
+      }
+
+      const { data } = await getBookList(params)
+      if (reset) setBook(data.documents)
+      else setBook(book.concat(data.documents))
+    },
+    [book]
+  )
+
+  const searchBookHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.currentTarget.value)
+    if (query.length > 0) getbookAPIHandler(query, true)
+  }
+
+  return (
+    <main>
+      <section>
+        <form>
+          <input type='text' placeholder='책제목을 입력해주세요!' onChange={searchBookHandler} />
+          <button type='submit'>검색</button>
+        </form>
+      </section>
+    </main>
+  )
 }
 
-export default TodoList
+export default Main
