ad955af3da9cb9309b87e093bce876bfeff1501e
doheek2 <doheek2@naver.com>
2022-10-23 18:56:14 +0900

    fix: DarkModeBtn 컴포넌트 분리

--- src/components/Bar/index.tsx
+++ src/components/Bar/index.tsx
@@ -1,29 +1,17 @@
 import { useDispatch, useSelector } from 'react-redux'
-import { useState } from 'react'
-
 import { HiMenu } from 'react-icons/hi'
-import { MdOutlineWbSunny } from 'react-icons/md'
-import { BsFillMoonFill } from 'react-icons/bs'
 
 import { storeType } from 'store'
 import { menuActions } from 'store/menu-slice'
+
 import MenuBar from 'components/MenuBar'
+import DarkModeBtn from 'components/darkModeBtn'
 import styles from './bar.module.scss'
 
 const Bar = () => {
   const dispatch = useDispatch()
-  const isMemuOpen = useSelector((state: storeType) => state.menu.isOpen)
-  const [isDarkTheme, setIsDarkTheme] = useState(false)
-
+  const isMemuOpen = useSelector((state: storeType) => state.menu.isMenuOpen)
   const menuBtnClickHandler = () => dispatch(menuActions.isOpenMenu())
-  const modeBtnClickHandler = () => setIsDarkTheme((prev) => !prev)
-
-  const darkThemeHandler = () => {
-    if (isDarkTheme) {
-      return <BsFillMoonFill className={styles.darkBtn} color='#020100' />
-    }
-    return <MdOutlineWbSunny className={styles.brightBtn} color='#020100' />
-  }
 
   return (
     <>
@@ -32,9 +20,7 @@ const Bar = () => {
           <HiMenu className={styles.menuBtn} color='#020100' />
         </button>
         <p className={styles.name}>LOOKBOOK</p>
-        <button type='button' className={styles.modeBtn} onClick={modeBtnClickHandler}>
-          {darkThemeHandler()}
-        </button>
+        <DarkModeBtn />
       </header>
       <div className={styles.menuBarContainer}>{isMemuOpen && <MenuBar />}</div>
     </>
