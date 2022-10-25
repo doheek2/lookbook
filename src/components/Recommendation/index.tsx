269d3720fae8127114411d73a9c5b9afaa8caa6a
doheek2 <doheek2@naver.com>
2022-10-25 22:15:56 +0900

    fix: booksData 가져올 때 isSuccess가 true 되는 버그 수정 및 출판사, 가격 정보 추가

--- src/components/Recommendation/index.tsx
+++ src/components/Recommendation/index.tsx
@@ -1,22 +1,33 @@
 import useRecommendationQueries from 'hooks/useRecommendationQueries'
 import styles from './recommendation.module.scss'
 
+// TODO :: image slice 기능 추가
 const Recommendation = () => {
   const booksData = useRecommendationQueries()
-  const isSuccess = booksData.some((result) => result.isSuccess)
+  const isGetDataSuccess = booksData.every((data) => data.isSuccess === true)
+
+  const numberCommas = (price: number) => {
+    return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+  }
 
   return (
     <section className={styles.recommendationContainer}>
       <p className={styles.title}>Front-end 책 추천</p>
       <article className={styles.bookListContainer}>
-        {isSuccess &&
+        {isGetDataSuccess &&
           booksData.map((v, i) => {
             const key = `recommendation${i}`
             const data = v.data?.data.documents[0]
+            const priceCommas = numberCommas(data.price)
+
             return (
-              <div key={key}>
+              <div key={key} className={styles.data}>
                 <img src={data.thumbnail} alt={data.title} />
-                <p>{data.title}</p>
+                <div className={styles.metaData}>
+                  <p className={styles.bookTitle}>{data.title}</p>
+                  <p>{data.publisher}</p>
+                  <p>{priceCommas}원</p>
+                </div>
               </div>
             )
           })}
