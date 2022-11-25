a2a4d6abd8913d2436056b83fb985c2db0570657
doheek2 <doheek2@naver.com>
2022-11-25 18:05:24 +0900

    feat: BookItem 컴포넌트 추가 및 image 에러 시 src 주소 변경

---
+++ src/components/BookItem/index.tsx
@@ -0,0 +1,35 @@
+import { SyntheticEvent, useCallback } from 'react'
+import cx from 'classnames'
+
+import notFoundImg from 'assets/pngs/notFoundImg.png'
+import styles from './bookItem.module.scss'
+
+interface IProps {
+  id: string
+  isList: boolean
+  thumbnail: string
+  title: string
+  publisher: string
+  price: string
+}
+
+const BookItem = ({ id, isList, thumbnail, title, publisher, price }: IProps) => {
+  const priceCommas = price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+
+  const imgErrorHandler = useCallback((e: SyntheticEvent<HTMLImageElement>) => {
+    e.currentTarget.src = notFoundImg
+  }, [])
+
+  return (
+    <div title={id} itemID={id} className={cx(styles.data, isList && styles.list)}>
+      <img src={thumbnail} alt={title} onError={imgErrorHandler} />
+      <div className={styles.metaData}>
+        <p className={styles.bookTitle}>{title}</p>
+        <p>{publisher}</p>
+        <p>{priceCommas}원</p>
+      </div>
+    </div>
+  )
+}
+
+export default BookItem
