5a74b10de81aa59bf7bfedaedd4ef2f04a893840
doheek2 <doheek2@naver.com>
2022-11-28 21:52:12 +0900

    feat: Button 컴포넌트 추가

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,27 @@
+import { MouseEventHandler } from 'react'
+import cx from 'classnames'
+
+import styles from './button.module.scss'
+
+interface IProps {
+  children: JSX.Element
+  isSubmit: boolean
+  className?: string
+  onClick?: MouseEventHandler<HTMLButtonElement>
+  disabled?: boolean
+}
+
+const Button = ({ children, isSubmit, className, onClick, disabled }: IProps) => {
+  return (
+    <button
+      type={isSubmit ? 'submit' : 'button'}
+      className={cx(styles.button, className)}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button
