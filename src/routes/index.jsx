32bd5f0ffd18ebc5613ac82218b21be4becf555b
doheek2 <doheek2@naver.com>
2022-11-28 21:58:50 +0900

    feat: login, signup route 추가

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,16 +1,21 @@
 import { Routes, Route } from 'react-router-dom'
-import styles from './Routes.module.scss'
 
 import Main from './Main'
+import Login from './Login'
+import SignUp from './SignUp'
 import BookDiary from './BookDiary'
 import LookBook from './LookBook'
 import Favorites from './Favorites'
+
+import styles from './Routes.module.scss'
 
 const App = () => {
   return (
     <div className={styles.container}>
       <Routes>
         <Route path='/' element={<Main />} />
+        <Route path='login' element={<Login />} />
+        <Route path='signup' element={<SignUp />} />
         <Route path='bookdiary' element={<BookDiary />} />
         <Route path='lookbook' element={<LookBook />} />
         <Route path='favorites' element={<Favorites />} />
