b5454a2bd6e7858225ea457036cfa409628e377c
doheek2 <doheek2@naver.com>
2022-11-29 22:05:53 +0900

    feat: firebase 연결 및 초기화

---
+++ src/firebaseApp/config.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+}
+
+const app = initializeApp(firebaseConfig)
+const appAuth = getAuth()
+const db = getFirestore(app)
+
+export { appAuth, db }
