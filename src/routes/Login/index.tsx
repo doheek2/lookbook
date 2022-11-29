b2416b57eb0227d51be2190b0b0539c99c89bf44
doheek2 <doheek2@naver.com>
2022-11-29 22:10:48 +0900

    fix: input value 삭제, 영문에서 한글로 변경, 로고 추가하여 클릭 시 메인 화면으로 이동, 패스워드 1글자만 입력해도 disable true로 변경

--- src/routes/Login/index.tsx
+++ src/routes/Login/index.tsx
@@ -1,80 +1,84 @@
 import { useCallback, ChangeEvent, useState, useEffect } from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 import cx from 'classnames'
 
 import Input from 'components/Input'
 import Button from 'components/Button'
+import SignContainer from 'components/SignContainer'
 
 import styles from './login.module.scss'
 
 const Login = () => {
+  const navigate = useNavigate()
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [isShowEmailText, setIsShowEmailText] = useState(true)
   const [isDisabledBtn, setIsDisabledBtn] = useState(true)
 
-  const inputChangeHandler = useCallback((e: ChangeEvent<HTMLInputElement>, isEmail: boolean) => {
+  const logoBtnClickHandler = useCallback(() => navigate('/'), [navigate])
+
+  const emailChangeHandler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     const { value } = e.currentTarget
-    if (isEmail) {
-      const regex =
-        // eslint-disable-next-line no-useless-escape
-        /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
-      if (regex.test(value)) setIsShowEmailText(true)
-      else setIsShowEmailText(false)
-      setEmail(value)
-    } else {
-      setPassword(value)
-    }
+    const regex =
+      // eslint-disable-next-line no-useless-escape
+      /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+
+    if (regex.test(value)) setIsShowEmailText(true)
+    else setIsShowEmailText(false)
+
+    setEmail(value)
+  }, [])
+
+  const passwordChangeHandler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.currentTarget.value)
   }, [])
 
   useEffect(() => {
-    const passLen = password.length
-    if (isShowEmailText && passLen > 8 && passLen < 20) setIsDisabledBtn(false)
+    if (isShowEmailText && password) setIsDisabledBtn(false)
     else setIsDisabledBtn(true)
-  }, [isShowEmailText, password.length])
+  }, [isShowEmailText, password])
 
   return (
-    <main className={styles.loginWrapper}>
-      <article className={styles.whiteBox}>
+    <SignContainer logoBtnClickHandler={logoBtnClickHandler}>
+      <article className={styles.wrapper}>
         <div>
-          <h3>Welcome Back !</h3>
-          <span className={styles.loginText}>
-            We happy to see you again. <br />
-            To use your account, you should log in first.
-          </span>
+          <h3>로그인</h3>
+          <button type='button' className={styles.mobileLogo} onClick={logoBtnClickHandler}>
+            LOOKBOOK
+          </button>
         </div>
         <form className={styles.loginForm}>
           <Input
             type='text'
+            isAutocomplete
             className={styles.input}
-            value={email}
-            placeholder='Email'
-            onChange={(e) => inputChangeHandler(e, true)}
+            placeholder='이메일'
+            onChange={emailChangeHandler}
           />
           {!isShowEmailText && <p className={styles.validate}>이메일 형식이 틀렸습니다.</p>}
           <Input
             type='password'
+            isAutocomplete={false}
             className={styles.input}
-            value={password}
-            placeholder='Password'
-            onChange={(e) => inputChangeHandler(e, false)}
+            placeholder='비밀번호'
+            onChange={passwordChangeHandler}
           />
           <Button
             isSubmit
             className={cx(styles.loginBtn, !isDisabledBtn && styles.disabledBtn)}
             disabled={isDisabledBtn}
           >
-            <p>LOGIN</p>
+            <p>로그인</p>
           </Button>
         </form>
         <div className={styles.signUpTextWrapper}>
-          <p className={styles.signUpAccountText}>Don&apos;t have an account?</p>
+          <p className={styles.signUpAccountText}>계정이 없으신가요?</p>
           <NavLink to='/signUp' className={styles.signUpText}>
-            Sign Up
+            회원가입
           </NavLink>
         </div>
       </article>
-    </main>
+    </SignContainer>
   )
 }
 
