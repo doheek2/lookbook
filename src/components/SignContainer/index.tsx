51791b370555227caae0631e583a6b8381776197
doheek2 <doheek2@naver.com>
2022-11-29 22:04:11 +0900

    feat: 회원가입/로그인 공용 컨테이너 컴포넌트 생성

---
+++ src/components/SignContainer/index.tsx
@@ -0,0 +1,24 @@
+import { MouseEventHandler } from 'react'
+import styles from './signContainer.module.scss'
+
+interface IProps {
+  children: JSX.Element
+  logoBtnClickHandler: MouseEventHandler<HTMLButtonElement>
+}
+
+const SignContainer = ({ children, logoBtnClickHandler }: IProps) => {
+  return (
+    <main className={styles.loginWrapper}>
+      <article className={styles.introduce}>
+        <p>책으로 소통하는</p>
+        <p>우리만의 공간</p>
+        <button type='button' className={styles.webLogo} onClick={logoBtnClickHandler}>
+          LOOKBOOK
+        </button>
+      </article>
+      <article className={styles.whiteBox}>{children}</article>
+    </main>
+  )
+}
+
+export default SignContainer
