d255f1aa9bc5d6fa5ce9147b4cf267d53a9119cc
doheek2 <doheek2@naver.com>
2022-12-03 19:56:58 +0900

    feat: 로그인/로그아웃 버튼 추가

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,13 +1,19 @@
 import { useCallback } from 'react'
 import { NavLink, useNavigate } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import cx from 'classnames'
 
 import { FaHome, FaBook, FaHeart } from 'react-icons/fa'
 import { BsFillPeopleFill } from 'react-icons/bs'
+import { HiLogin, HiLogout } from 'react-icons/hi'
 
+import { storeType } from 'store'
 import { menuActions } from 'store/menu-slice'
+import { IUserState } from 'types/firebaseAuth'
+import useLogout from 'hooks/useLogout'
 import DarkModeBtn from 'components/DarkModeBtn'
+import Button from 'components/Button'
+
 import styles from './menu.module.scss'
 
 interface IProps {
@@ -17,9 +23,15 @@ interface IProps {
 const Menu = ({ isMobile }: IProps) => {
   const dispatch = useDispatch()
   const navigate = useNavigate()
+  const { logoutHandler } = useLogout()
+  const user: IUserState | null = useSelector((state: storeType) => state.auth.user)
 
   const logoBtnClickHandler = useCallback(() => {
     navigate('/')
+  }, [navigate])
+
+  const loginBtnClickHandler = useCallback(() => {
+    navigate('/login')
   }, [navigate])
 
   const menuBarCloseHandler = useCallback(() => {
@@ -79,6 +91,37 @@ const Menu = ({ isMobile }: IProps) => {
             <FaHeart />
             <span>즐겨찾기</span>
           </NavLink>
+        </li>
+        <div className={styles.line} />
+        <li>
+          {!user && (
+            <Button
+              isSubmit={false}
+              onClick={() => {
+                if (isMobile) menuBarCloseHandler()
+                loginBtnClickHandler()
+              }}
+            >
+              <>
+                <HiLogin />
+                <span>로그인</span>
+              </>
+            </Button>
+          )}
+          {user && (
+            <Button
+              isSubmit={false}
+              onClick={() => {
+                if (isMobile) menuBarCloseHandler()
+                logoutHandler()
+              }}
+            >
+              <>
+                <HiLogout />
+                <span>로그아웃</span>
+              </>
+            </Button>
+          )}
         </li>
       </ul>
       <div className={styles.darkModeBtnContainer}>
