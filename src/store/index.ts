6586fcd744f5efab90f7e68dcb21216b52f666d0
doheek2 <doheek2@naver.com>
2022-12-03 20:05:58 +0900

    feat: auth 관련 reducer 추가

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import menuSlice from './menu-slice'
+import authSlice from './auth-slice'
 
 const store = configureStore({
-  reducer: { menu: menuSlice.reducer },
+  reducer: { menu: menuSlice.reducer, auth: authSlice.reducer },
 })
 
 export type storeType = ReturnType<typeof store.getState>
