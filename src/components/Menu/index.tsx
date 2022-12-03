92260b319aa64c39c701dec91f7647a1d59b480f
doheek2 <doheek2@naver.com>
2022-12-03 21:02:03 +0900

    refactor: NavItem & SignItem 컴포넌트 추가

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,18 +1,16 @@
 import { useCallback } from 'react'
-import { NavLink, useNavigate } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import cx from 'classnames'
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 import { FaHome, FaBook, FaHeart } from 'react-icons/fa'
 import { BsFillPeopleFill } from 'react-icons/bs'
 import { HiLogin, HiLogout } from 'react-icons/hi'
 
 import { storeType } from 'store'
-import { menuActions } from 'store/menu-slice'
 import { IUserState } from 'types/firebaseAuth'
-import useLogout from 'hooks/useLogout'
 import DarkModeBtn from 'components/DarkModeBtn'
-import Button from 'components/Button'
+import NavItem from './NavItem'
+import SignItem from './SignItem'
 
 import styles from './menu.module.scss'
 
@@ -21,22 +19,12 @@ interface IProps {
 }
 
 const Menu = ({ isMobile }: IProps) => {
-  const dispatch = useDispatch()
   const navigate = useNavigate()
-  const { logoutHandler } = useLogout()
   const user: IUserState | null = useSelector((state: storeType) => state.auth.user)
 
   const logoBtnClickHandler = useCallback(() => {
     navigate('/')
   }, [navigate])
-
-  const loginBtnClickHandler = useCallback(() => {
-    navigate('/login')
-  }, [navigate])
-
-  const menuBarCloseHandler = useCallback(() => {
-    dispatch(menuActions.isOpenMenu())
-  }, [dispatch])
 
   return (
     <nav className={styles.sideBarContainer}>
@@ -44,84 +32,14 @@ const Menu = ({ isMobile }: IProps) => {
         LOOKBOOK
       </button>
       <ul className={styles.menuContainer}>
-        <li>
-          <NavLink
-            to='/'
-            onClick={() => {
-              if (isMobile) menuBarCloseHandler()
-            }}
-            className={({ isActive }) => cx({ [styles.isActive]: isActive })}
-          >
-            <FaHome />
-            <span>홈</span>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to='/bookdiary'
-            onClick={() => {
-              if (isMobile) menuBarCloseHandler()
-            }}
-            className={({ isActive }) => cx({ [styles.isActive]: isActive })}
-          >
-            <FaBook />
-            <span>책일기장</span>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to='/lookbook'
-            onClick={() => {
-              if (isMobile) menuBarCloseHandler()
-            }}
-            className={({ isActive }) => cx({ [styles.isActive]: isActive })}
-          >
-            <BsFillPeopleFill />
-            <span>LOOKBOOK</span>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to='/favorites'
-            onClick={() => {
-              if (isMobile) menuBarCloseHandler()
-            }}
-            className={({ isActive }) => cx({ [styles.isActive]: isActive })}
-          >
-            <FaHeart />
-            <span>즐겨찾기</span>
-          </NavLink>
-        </li>
+        <NavItem isMobile={isMobile} route='/' svg={<FaHome />} name='홈' />
+        <NavItem isMobile={isMobile} route='/bookdiary' svg={<FaBook />} name='책일기장' />
+        <NavItem isMobile={isMobile} route='/lookbook' svg={<BsFillPeopleFill />} name='LOOKBOOK' />
+        <NavItem isMobile={isMobile} route='/favorites' svg={<FaHeart />} name='즐겨찾기' />
         <div className={styles.line} />
         <li>
-          {!user && (
-            <Button
-              isSubmit={false}
-              onClick={() => {
-                if (isMobile) menuBarCloseHandler()
-                loginBtnClickHandler()
-              }}
-            >
-              <>
-                <HiLogin />
-                <span>로그인</span>
-              </>
-            </Button>
-          )}
-          {user && (
-            <Button
-              isSubmit={false}
-              onClick={() => {
-                if (isMobile) menuBarCloseHandler()
-                logoutHandler()
-              }}
-            >
-              <>
-                <HiLogout />
-                <span>로그아웃</span>
-              </>
-            </Button>
-          )}
+          {!user && <SignItem isMobile={isMobile} isLogin svg={<HiLogin />} name='로그인' />}
+          {user && <SignItem isMobile={isMobile} isLogin={false} svg={<HiLogout />} name='로그아웃' />}
         </li>
       </ul>
       <div className={styles.darkModeBtnContainer}>
