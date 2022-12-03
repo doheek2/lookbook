903d30115cb5eea294e874832490e25d90d11fe0
doheek2 <doheek2@naver.com>
2022-12-03 20:01:36 +0900

    fix: 회원가입 후 메인화면으로 이동 기능 추가

--- src/hooks/useSignUp.ts
+++ src/hooks/useSignUp.ts
@@ -1,9 +1,14 @@
 import { useState } from 'react'
 import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 import { appAuth } from 'firebaseApp/config'
+import { authActions } from 'store/auth-slice'
 
 const useSignUp = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
   const [error, setError] = useState(null)
   const [isPending, setIsPending] = useState(false)
 
@@ -14,12 +19,12 @@ const useSignUp = () => {
     createUserWithEmailAndPassword(appAuth, email, password)
       .then((userCredential) => {
         const { user } = userCredential
-        console.log(user)
 
         if (!user) throw new Error('회원가입 실패')
 
         updateProfile(user, { displayName })
           .then(() => {
+            dispatch(authActions.user({ displayName, email, uid: user.uid }))
             setError(null)
             setIsPending(false)
           })
@@ -27,6 +32,8 @@ const useSignUp = () => {
             setError(err.message)
             setIsPending(false)
           })
+
+        navigate('/')
       })
       .catch((err) => {
         setError(err.message)
