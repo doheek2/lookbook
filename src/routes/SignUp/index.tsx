e6e83c418db4b670cb54c71dabb35262496eb9e0
doheek2 <doheek2@naver.com>
2022-12-08 02:01:15 +0900

    feat: 닉네임 & 비밀번호 유효성 검사 추가에 따른 리팩토링, Input 컴포넌트 validateText 추가

--- src/routes/SignUp/index.tsx
+++ src/routes/SignUp/index.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FormEvent, useCallback, useEffect, useState } from 'react'
+import { ChangeEvent, FormEvent, useCallback, useState } from 'react'
 import { NavLink, useNavigate } from 'react-router-dom'
 import cx from 'classnames'
 
@@ -12,16 +12,37 @@ import styles from './signUp.module.scss'
 const SignUp = () => {
   const navigate = useNavigate()
   const { error, isPending, signupHandler } = useSignUp()
+
+  // 값 저장
   const [displayName, setDisplayName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const [isShowEmailText, setIsShowEmailText] = useState(true)
-  const [isDisabledBtn, setIsDisabledBtn] = useState(true)
+
+  // 에러메세지 저장
+  const [displayNameMsg, setdisplayNameMsg] = useState<string | null>(null)
+  const [emailMsg, setemailMsg] = useState<string | null>(null)
+  const [passwordMsg, setPasswordMsg] = useState<string | null>(null)
+
+  // 유효성 검사
+  const [isDisplayName, setIsDisplayName] = useState(false)
+  const [isEmail, setIsEmail] = useState(false)
+  const [isPassWord, setIsPassWord] = useState(false)
 
   const logoBtnClickHandler = useCallback(() => navigate('/'), [navigate])
 
   const displayNameChangeHandler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    setDisplayName(e.currentTarget.value)
+    const { value } = e.currentTarget
+    const displayNameLength = value.length
+
+    if (displayNameLength < 2 || displayNameLength > 10) {
+      setdisplayNameMsg('닉네임은 2~10자로 입력해주세요.')
+      setIsDisplayName(false)
+    } else {
+      setdisplayNameMsg(null)
+      setIsDisplayName(true)
+    }
+
+    setDisplayName(value)
   }, [])
 
   const emailChangeHandler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
@@ -30,14 +51,30 @@ const SignUp = () => {
       // eslint-disable-next-line no-useless-escape
       /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
 
-    if (regex.test(value)) setIsShowEmailText(true)
-    else setIsShowEmailText(false)
+    if (regex.test(value)) {
+      setemailMsg(null)
+      setIsEmail(true)
+    } else {
+      setemailMsg('이메일 형식이 틀렸습니다.')
+      setIsEmail(false)
+    }
 
     setEmail(value)
   }, [])
 
   const passwordChangeHandler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.currentTarget.value)
+    const { value } = e.currentTarget
+    const passwordLength = value.length
+
+    if (passwordLength < 8 || passwordLength > 64) {
+      setPasswordMsg('비밀번호는 8~64자로 입력해주세요.')
+      setIsPassWord(false)
+    } else {
+      setPasswordMsg(null)
+      setIsPassWord(true)
+    }
+
+    setPassword(value)
   }, [])
 
   const submitHandler = useCallback(
@@ -48,11 +85,6 @@ const SignUp = () => {
     [displayName, email, password, signupHandler]
   )
 
-  useEffect(() => {
-    if (isShowEmailText && displayName && password) setIsDisabledBtn(false)
-    else setIsDisabledBtn(true)
-  }, [displayName, isShowEmailText, password])
-
   return (
     <SignContainer logoBtnClickHandler={logoBtnClickHandler}>
       <article className={styles.wrapper}>
@@ -62,40 +94,40 @@ const SignUp = () => {
             LOOKBOOK
           </button>
         </div>
-        <form className={styles.loginForm} onSubmit={submitHandler}>
+        <form className={styles.signUpForm} onSubmit={submitHandler}>
           <Input
             type='text'
             isAutocomplete
-            className={styles.input}
             placeholder='닉네임'
+            validateText={displayNameMsg}
             onChange={displayNameChangeHandler}
           />
           <Input
             type='text'
             isAutocomplete
-            className={styles.input}
             placeholder='이메일'
+            validateText={emailMsg}
             onChange={emailChangeHandler}
           />
-          {!isShowEmailText && <p className={styles.validate}>이메일 형식이 틀렸습니다.</p>}
           <Input
             type='password'
             isAutocomplete={false}
-            className={styles.input}
             placeholder='비밀번호'
+            validateText={passwordMsg}
             onChange={passwordChangeHandler}
           />
+          {error && <p className={styles.errorMsg}>{error}</p>}
           <Button
             isSubmit
-            className={cx(styles.loginBtn, !isDisabledBtn && styles.disabledBtn)}
-            disabled={isDisabledBtn}
+            className={cx(styles.signUpBtn, isDisplayName && isEmail && isPassWord && styles.disabledBtn)}
+            disabled={!(isDisplayName && isEmail && isPassWord)}
           >
             <p>가입하기</p>
           </Button>
         </form>
-        <div className={styles.signUpTextWrapper}>
-          <p className={styles.signUpAccountText}>계정이 있으신가요?</p>
-          <NavLink to='/login' className={styles.signUpText}>
+        <div className={styles.loginWrapper}>
+          <p className={styles.loginAccountText}>계정이 있으신가요?</p>
+          <NavLink to='/login' className={styles.loginText}>
             로그인
           </NavLink>
         </div>
