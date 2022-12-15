6f7277b5f123f0ae3ac1dfc76790d8f11d892796
doheek2 <doheek2@naver.com>
2022-12-16 00:03:18 +0900

    feat: 계정 정보 없을 때 NotLoginForm 출력

--- src/routes/LookBook/index.tsx
+++ src/routes/LookBook/index.tsx
@@ -1,10 +1,21 @@
+import { useSelector } from 'react-redux'
+
+import { storeType } from 'store'
+import { IUserState } from 'types/firebaseAuth'
 import Container from 'components/Container'
+import NotLoginForm from 'components/NotLoginForm'
+
 import styles from './lookbook.module.scss'
 
 const LookBook = () => {
+  const user: IUserState | null = useSelector((state: storeType) => state.auth.user)
+
   return (
     <Container>
-      <p>lookbook</p>
+      <main className={styles.lookbookWrapper}>
+        {user && <p>로그인</p>}
+        {!user && <NotLoginForm pageName='LOOKBOOK' />}
+      </main>
     </Container>
   )
 }
