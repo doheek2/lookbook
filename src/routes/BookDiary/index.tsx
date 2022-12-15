ce81ab88f0fc552986add64892439d608a3d4825
doheek2 <doheek2@naver.com>
2022-12-15 23:59:36 +0900

    feat: 계정 정보 없을 때 NotLoginForm 출력

--- src/routes/BookDiary/index.tsx
+++ src/routes/BookDiary/index.tsx
@@ -1,9 +1,21 @@
+import { useSelector } from 'react-redux'
+
+import { storeType } from 'store'
+import { IUserState } from 'types/firebaseAuth'
 import Container from 'components/Container'
+import NotLoginForm from 'components/NotLoginForm'
+
+import styles from './bookDiary.module.scss'
 
 const BookDiary = () => {
+  const user: IUserState | null = useSelector((state: storeType) => state.auth.user)
+
   return (
     <Container>
-      <p>Favorites</p>
+      <main className={styles.diaryWrapper}>
+        {user && <p>로그인</p>}
+        {!user && <NotLoginForm pageName='책일기장' />}
+      </main>
     </Container>
   )
 }
