9fc53833de4718374073819f16d7dab7348f3a0a
doheek2 <doheek2@naver.com>
2022-12-19 22:18:09 +0900

    refactor: getPriceToCommas 함수로 변경

--- src/components/BookItem/index.tsx
+++ src/components/BookItem/index.tsx
@@ -2,6 +2,8 @@ import { SyntheticEvent, useCallback } from 'react'
 import cx from 'classnames'
 
 import notFoundImg from 'assets/pngs/notFoundImg.png'
+import { getPriceToCommas } from 'utils/lookbook'
+
 import styles from './bookItem.module.scss'
 
 interface IProps {
@@ -14,8 +16,7 @@ interface IProps {
 }
 
 const BookItem = ({ id, isList, thumbnail, title, publisher, price }: IProps) => {
-  const priceCommas = price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-
+  const priceCommas = getPriceToCommas(`${price}`)
   const imgErrorHandler = useCallback((e: SyntheticEvent<HTMLImageElement>) => {
     e.currentTarget.src = notFoundImg
   }, [])
