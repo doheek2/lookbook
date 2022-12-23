8262a15bc9e9890a61ff96c299e6dd8b228c2dcb
doheek2 <doheek2@naver.com>
2022-12-23 21:59:18 +0900

    refactor: 버틑 내용 변경 및 onClick 추가

--- src/components/NoDataForm/index.tsx
+++ src/components/NoDataForm/index.tsx
@@ -1,8 +1,10 @@
+import { useCallback } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { FaBook, FaHeart } from 'react-icons/fa'
-import { BsFillPeopleFill, BsPlusLg } from 'react-icons/bs'
+import { BsFillPeopleFill } from 'react-icons/bs'
 
-import styles from './noDataForm.module.scss'
 import Button from 'components/Button'
+import styles from './noDataForm.module.scss'
 
 interface IProps {
   menu: string
@@ -15,13 +17,16 @@ const NoDataForm = ({ menu }: IProps) => {
     즐겨찾기: <FaHeart />,
   }[menu]
 
+  const navigate = useNavigate()
+  const openFormBtnClickHandler = useCallback(() => navigate('/diaryForm', { state: { isEdit: false } }), [navigate])
+
   return (
     <main className={styles.noDataWrapper}>
       <div className={styles.iconBackground}>{svgIcon}</div>
       <p>등록된 {menu}이 없습니다.</p>
       {menu === '책일기장' && (
-        <Button isSubmit={false} className={styles.openFormBtn}>
-          <BsPlusLg />
+        <Button isSubmit={false} className={styles.writeDiaryBtn} onClick={openFormBtnClickHandler}>
+          일기쓰기
         </Button>
       )}
     </main>
