b5d9d0a9e90c1b04b242a26f3ee54061b66e4453
doheek2 <doheek2@naver.com>
2022-12-23 22:02:16 +0900

    feat: openFormBtn 추가 및 onClick 지정

--- src/routes/BookDiary/index.tsx
+++ src/routes/BookDiary/index.tsx
@@ -1,17 +1,32 @@
+import { useCallback } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { BsPlusLg } from 'react-icons/bs'
+
 import Container from 'components/Container'
 import NotLoginForm from 'components/NotLoginForm'
+import Button from 'components/Button'
+import NoDataForm from 'components/NoDataForm'
 import useAuth from 'hooks/useAuth'
 
 import styles from './bookDiary.module.scss'
-import NoDataForm from 'components/NoDataForm'
 
 const BookDiary = () => {
   const { user } = useAuth()
+  const navigate = useNavigate()
+
+  const openFormBtnClickHandler = useCallback(() => navigate('/diaryForm', { state: { isEdit: false } }), [navigate])
 
   return (
     <Container>
       <main className={styles.diaryWrapper}>
-        {user && <NoDataForm menu='책일기장' />}
+        {user && (
+          <div>
+            <NoDataForm menu='책일기장' />
+            <Button isSubmit={false} className={styles.mobileOpenFormBtn} onClick={openFormBtnClickHandler}>
+              <BsPlusLg />
+            </Button>
+          </div>
+        )}
         {!user && <NotLoginForm pageName='책일기장' />}
       </main>
     </Container>
