6b39e6432c6989d822202d6fced32c332c5967b1
doheek2 <doheek2@naver.com>
2022-12-28 23:31:38 +0900

    feat: inputChangeHandler, textareaChangeHandler 추가

--- src/routes/DiaryForm/InputContainer/index.tsx
+++ src/routes/DiaryForm/InputContainer/index.tsx
@@ -1,3 +1,7 @@
+import { useCallback, ChangeEvent } from 'react'
+import { useDispatch } from 'react-redux'
+
+import { diaryActions } from 'store/diary-slice'
 import styles from './inputContainer.module.scss'
 
 interface IProps {
@@ -7,11 +11,29 @@ interface IProps {
 }
 
 const InputContainer = ({ title, id, isInput }: IProps) => {
+  const dispatch = useDispatch()
+
+  const inputChangeHandler = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      const { value } = e.currentTarget
+      if (id === 'title') dispatch(diaryActions.title(value))
+      else if (id === 'comment') dispatch(diaryActions.content(value))
+    },
+    [dispatch, id]
+  )
+
+  const textareaChangeHandler = useCallback(
+    (e: ChangeEvent<HTMLTextAreaElement>) => {
+      dispatch(diaryActions.comment(e.currentTarget.value))
+    },
+    [dispatch]
+  )
+
   return (
     <article className={styles.inputWrapper}>
       <label htmlFor={id}>{title}</label>
-      {isInput && <input type='text' id={id} />}
-      {!isInput && <textarea id={id} />}
+      {isInput && <input type='text' id={id} onChange={inputChangeHandler} />}
+      {!isInput && <textarea id={id} onChange={textareaChangeHandler} />}
     </article>
   )
 }
