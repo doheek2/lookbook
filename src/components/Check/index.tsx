49dc681cd992f2b00e3cf46c339f8b1ec4b3cd49
doheek2 <doheek2@naver.com>
2022-12-28 23:23:25 +0900

    feat: input check 기능 추가, props 수정

--- src/components/Check/index.tsx
+++ src/components/Check/index.tsx
@@ -1,14 +1,27 @@
+import { useCallback } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import cx from 'classnames'
 
 import { CheckIcon } from 'assets/svgs'
+import { storeType } from 'store'
 import styles from './check.module.scss'
+import { diaryActions } from 'store/diary-slice'
 
 interface IProps {
   size: string
   id: string
-  isChecked: boolean
+  value: number
 }
-const Check = ({ size, id, isChecked }: IProps) => {
+const Check = ({ size, id, value }: IProps) => {
+  const dispatch = useDispatch()
+  const isdiaryOpens = useSelector((state: storeType) => state.diary.isdiaryOpens)
+
+  const inputChangeHandler = useCallback(() => {
+    const initArr = [false, false]
+    initArr[value] = true
+    dispatch(diaryActions.isdiaryOpens(initArr))
+  }, [dispatch, value])
+
   return (
     <div
       className={cx(
@@ -17,7 +30,7 @@ const Check = ({ size, id, isChecked }: IProps) => {
         size === 'large' && styles.large
       )}
     >
-      <input type='checkbox' id={id} checked={isChecked} />
+      <input type='checkbox' id={id} checked={isdiaryOpens[value]} onChange={inputChangeHandler} />
       <CheckIcon fill='#ffffff' stroke='#ffffff' strokeWidth='15' />
     </div>
   )
