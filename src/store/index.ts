cfee8ad18a8d6724d63a3540df6cbf3929c9cbfb
doheek2 <doheek2@naver.com>
2022-12-28 23:28:57 +0900

    feat: diary reducer 추가

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,9 +2,10 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import menuSlice from './menu-slice'
 import authSlice from './auth-slice'
+import diarySlice from './diary-slice'
 
 const store = configureStore({
-  reducer: { menu: menuSlice.reducer, auth: authSlice.reducer },
+  reducer: { menu: menuSlice.reducer, auth: authSlice.reducer, diary: diarySlice.reducer },
 })
 
 export type storeType = ReturnType<typeof store.getState>
